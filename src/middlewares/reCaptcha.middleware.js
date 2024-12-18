de5a9f966f1e66a236c47b06a35888ea206ed04c
Md. Saif Ali <159515806+mdsaifalidev@users.noreply.github.com>
2024-12-18 11:05:29 +0530

    Update reCaptcha.middleware.js

--- src/middlewares/reCaptcha.middleware.js
+++ src/middlewares/reCaptcha.middleware.js
@@ -15,7 +15,7 @@ const verifyReCaptcha = asyncHandler(async (req, res, next) => {
   }
 
   const url = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${reCaptcha}`
-  const {data} = await axios.get(url)
+  const {data} = await axios.post(url)
 
   if (!data.success) {
     throw new ApiError(400, "reCaptcha verification failed")
