b920d5986d67e9ccc127e9a1b53b1f92709d0c1e
Md. Saif Ali <159515806+mdsaifalidev@users.noreply.github.com>
2024-12-19 18:35:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(helmet())
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://mockaiinterview.vercel.app",
     credentials: true,
   })
 )
